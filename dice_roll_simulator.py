bf062f770a8d326bfb76acbbd66a69b85d0c1cb1
Patrick <52718649+pbrebner@users.noreply.github.com>
2021-05-16 20:52:38 -0400

    Updated Output Message

--- dice_roll_simulator.py
+++ dice_roll_simulator.py
@@ -1,5 +1,5 @@
 # Simulates the roll of a 6 sided die
 import random
 
-while int(input("Press 1 to roll the dice or 0 to exit:\n")):
-    print(random.randint(1, 6))
+while int(input("Press 1 to roll the die or 0 to exit:\n")):
+    print("Die Roll: ", random.randint(1, 6))
