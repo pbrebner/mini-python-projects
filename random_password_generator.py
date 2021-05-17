a03b68b07642adf7842edc49e61fee1ff310bc67
Patrick <52718649+pbrebner@users.noreply.github.com>
2021-05-16 21:29:36 -0400

    Added Comments. Ran and Tested

--- random_password_generator.py
+++ random_password_generator.py
@@ -2,7 +2,9 @@
 
 import random
 
+# User Input
 pass_length = int(input("Enter the length of the desired password: "))
 characters = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# Join together the random selected characters of length pass_length with "" as the sperator
 password = "".join(random.sample(characters, pass_length))
 print(password)
