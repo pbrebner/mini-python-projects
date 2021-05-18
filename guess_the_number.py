8f4ae3f50413166cc0aad92e956783057e199664
Patrick <52718649+pbrebner@users.noreply.github.com>
2021-05-17 20:35:56 -0400

    Fixed Errors. Ran and Tested

--- guess_the_number.py
+++ guess_the_number.py
@@ -6,10 +6,11 @@ import random
 
 number = random.randint(1, 10)
 for i in range(0, 3):
-    user_guess = int(input("Guess the Number: "))
+    user_guess = int(input("Guess the Number [1-10]: "))
     if user_guess == number:
         print("You Win!")
-        print("You guess the number right")
+        print("You guessed the correct number")
+        break
     elif user_guess > number:
         print("Your guess is too high")
     elif user_guess < number:
