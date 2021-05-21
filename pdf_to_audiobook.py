af49e2d1c61e7ee7921bc24156c36ba403049d2d
Patrick <52718649+pbrebner@users.noreply.github.com>
2021-05-20 21:01:21 -0400

    Ran pyttsx3 test

--- pdf_to_audiobook.py
+++ pdf_to_audiobook.py
@@ -1,6 +1,11 @@
 # Python script that takes a pdf input and converts the text to speech
-# Uses the pyttsx3 and PyPDF2
+# Uses the pyttsx3-2.71 and PyPDF2
 
 import pyttsx3, PyPDF2
 
-pdf_reader = PyPDF2.PdfFileReader(open("file.pdf", "rb"))
+# Test Text to voice conversion
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()
+
+# pdf_reader = PyPDF2.PdfFileReader(open("file.pdf", "rb"))
